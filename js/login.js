6d3a5a57771a11709c3ae7df1d10c854a1e23209
Danilo Ramos <danilocr@al.insper.edu.br>
2024-11-14 19:29:58 -0300

    login_teste

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const user = document.getElementById("Usuario").value;
+    const password = document.querySelector("input[type='password']").value;
+
+    localStorage.setItem("user", user);
+    localStorage.setItem("password", password);
+
+    console.log(user)
+    console.log(password)
+    window.location.href = "index.html";
+});
