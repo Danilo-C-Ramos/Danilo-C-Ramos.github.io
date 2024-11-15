69cdd1ef9753b7a2295e39d9dd77f7ee7196ff0d
Danilo Ramos <danilocr@al.insper.edu.br>
2024-11-15 19:21:22 -0300

    Começando a arrumar a atividade

---
+++ js/atividade.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alternativas = document.querySelectorAll('li')
+
+    alt_A = alternativas[0]
+    alt_B = alternativas[1]
+    alt_C = alternativas[2]
+    alt_D = alternativas[3]
+
+
+})
