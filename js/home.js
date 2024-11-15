ba5a86ff7c7a583ecc1a665cef879b5820d772fa
Danilo Ramos <danilocr@al.insper.edu.br>
2024-11-15 18:23:50 -0300

    login carregamento

---
+++ js/home.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    botoes = document.querySelectorAll("li")
+
+    nova_atividade = botoes[0]
+
+    nova_atividade.addEventListener('click', function(event) {
+        window.location.href = "atividade.html";
+    })
+
+    grafico = botoes[1]
+
+    linha_diagnostico = botoes[2]
+
+    linha = linha_diagnostico.querySelectorAll("li")
+
+    diagnostico = linha[0]
+
+    diagnostico.addEventListener('click', function(event) {
+        window.location.href = "diagnostico.html";
+    })
+
+    materias = linha[1]
+    materias.addEventListener('click', function(event) {
+        window.location.href = "materias.html";
+    })
+
+})
--- js/login.js
+++ js/login.js
@@ -1,13 +1,25 @@
-document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById("form").addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    const user = document.getElementById("Usuario").value;
-    const password = document.querySelector("input[type='password']").value;
+        const user = document.getElementById("Usuario").value;
+        const password = document.querySelector("input[type='password']").value;
 
-    localStorage.setItem("user", user);
-    localStorage.setItem("password", password);
+        localStorage.setItem("user", user);
+        localStorage.setItem("password", password);
 
-    console.log(user)
-    console.log(password)
-    window.location.href = "index.html";
+        console.log(user)
+        console.log(password)
+        
+        const overlay = document.getElementById("loading-overlay");
+        overlay.classList.remove("hidden");
+    
+        // Simula carregamento por 3 segundos
+        setTimeout(() => {
+        overlay.classList.add("hidden"); // Esconde o overlay após "carregamento"
+        window.location.href = "index.html";
+        }, 3000);
+    })
+    
 });
